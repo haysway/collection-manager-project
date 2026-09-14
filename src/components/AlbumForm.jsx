47e89868043ce7edb887a6bf2365ba672fe0941b
Hayden Swain-Schmit <swainschmity@gmail.com>
2026-09-14 16:38:34 -0600

    feat: form input now works, though entered albums are not saved anywhere. There is no validation on the form

--- src/components/AlbumForm.jsx
+++ src/components/AlbumForm.jsx
@@ -7,45 +7,176 @@ function AlbumForm(props) {
     // genres (multi select?)
     // record label
     // owned selector (yes/no)
-    // if yes, which media (vinyl, cd, cassette)
+    // if yes, which format (vinyl, cd, cassette)
+
+    // I had to research to see if a list can be used here
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredArtist, setEnteredArtist] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const [enteredGenres, setEnteredGenres] = useState([]);
+    const [enteredLabel, setEnteredLabel] = useState('');
+    const [enteredOwned, setEnteredOwned] = useState(false);
+    const [enteredFormat, setEnteredFormat] = useState([]);
+
+    const genres = [
+        'Rock',
+        'Alternative',
+        'Shoegaze',
+        'Indie',
+        'Punk',
+        'Metal',
+        'Pop',
+        'Jazz',
+        'Hip-Hop',
+    ];
+
+    const mediaFormats = [
+        'Vinyl',
+        'CD',
+        'Cassette',
+    ];
 
     const submitHandler = (event) => {
         event.preventDefault();
-    }
+
+        const albumData = {
+            name: enteredName,
+            artist: enteredArtist,
+            date: enteredDate,
+            genres: enteredGenres,
+            label: enteredLabel,
+            owned: enteredOwned,
+            formats: enteredFormat
+        };
+
+        props.onAddAlbum(albumData);
+
+        setEnteredName('');
+        setEnteredArtist('');
+        setEnteredDate('');
+        setEnteredGenres([]);
+        setEnteredLabel('');
+        setEnteredOwned(false);
+        setEnteredFormat([]);
+    };
 
     return (
         <form className="card" onSubmit={submitHandler}>
             <div className="form-control">
-                <label>Name</label>
+                <label>Name:</label>
+
+                <input
+                    type="text"
+                    value={enteredName}
+                    onChange={(e) => setEnteredName(e.target.value)}
+                />
             </div>
 
 
             <div className="form-control">
-                <label>Artist</label>
+                <label>Artist:</label>
+
+                <input
+                    type="text"
+                    value={enteredArtist}
+                    onChange={(e) => setEnteredArtist(e.target.value)}
+                />
             </div>
 
             <div className="form-control">
-                <label>Release Date</label>   
+                <label>Release Date:</label>   
+
+                <input
+                    type="date"
+                    value={enteredDate}
+                    onChange={(e) => setEnteredDate(e.target.value)}
+                />
             </div>
 
             <div className="form-control">
-                <label>Genres</label>                
+                <label>Genres:</label>
+
+                {/* I researched a bit on how checkboxes work, as the last time
+                I used them was in C#*/}
+                {genres.map((genre) => (
+                    <label key={genre}>
+                        <input
+                            type="checkbox"
+                            value={genre}
+                            checked={enteredGenres.includes(genre)}
+                            onChange={(e) => {
+                                if (e.target.checked) {
+                                    setEnteredGenres([...enteredGenres, genre]);
+                                } else {
+                                    setEnteredGenres(
+                                        enteredGenres.filter((g) => g !== genre)
+                                    );
+                                }
+                            }}  
+                        />
+                        {genre}
+                    </label>
+                ))}
             </div>
 
 
             <div className="form-control">
-                <label>Record Label</label>                
+                <label>Record Label:</label>  
+
+                <input
+                    type="text"
+                    value={enteredLabel}
+                    onChange={(e) => setEnteredLabel(e.target.value)}
+                />              
             </div>
 
 
             <div className="form-control">
-                <label>Owned</label>                
+                <label>Owned:</label>
+
+                <label>
+                    <input type="radio" name="owned" value="yes" checked={enteredOwned === true}
+                        onChange={(e) => setEnteredOwned(true)}  
+                    />
+                    Yes 
+                </label>   
+
+                <label>
+                    <input type="radio" name="owned" value="no" checked={enteredOwned === false}
+                        onChange={(e) => {
+                            setEnteredOwned(false);
+                            setEnteredFormat([]);
+                        }}  
+                    />
+                    No   
+                </label>    
             </div>
 
-
-            <div className="form-control">
-                <label>Format</label>                
-            </div>
+            {enteredOwned && (
+                <div className="form-control">
+                    <label>Format:</label>
+                    
+                    {mediaFormats.map((format) => (
+                        <label key={format}>
+                            <input
+                                type="checkbox"
+                                value={format}
+                                checked={enteredFormat.includes(format)}
+                                onChange={(e) => {
+                                    if (e.target.checked) {
+                                        setEnteredFormat([...enteredFormat, format]);
+                                    } else {
+                                        setEnteredFormat(
+                                            enteredFormat.filter((m) => m !== format)
+                                        );
+                                    }
+                                }}
+                            />
+                            {format}
+                        </label>
+                    ))}           
+                </div>
+            )}
 
             <button type="submit">Add Album</button>
         </form>
